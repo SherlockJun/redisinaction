30307208297bc50c9d325203ee366fa103364e8d
tanjianhui <tan_jianhui@163.com>
2017-02-15 17:25:28 +0800

    增加Redis5中数据类型基本操作的测试用例

--- src/test/java/redisinaction/c01/C01DemoTest.java
+++ src/test/java/redisinaction/c01/C01DemoTest.java
@@ -1,5 +1,6 @@
 package redisinaction.c01;
 
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import redis.clients.jedis.Jedis;
@@ -16,6 +17,7 @@ import java.util.logging.Logger;
  * Created by tanjianhui on 2017/2/14.
  */
 public class C01DemoTest {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(C01DemoTest.class);
 
     private Jedis jedis;
     private C01Demo c01Demo;
@@ -24,6 +26,82 @@ public class C01DemoTest {
     public void setup(){
         jedis = new Jedis("10.10.28.222", 6379);
         c01Demo = new C01Demo();
+    }
+
+
+
+    @Test
+    public void testRedisString(){
+        jedis.set("test", "Hello, World!");
+        logger.info("[key:test,value:{}]",jedis.get("test"));
+        jedis.del("test");
+        logger.info("[key:test,value:{}]",jedis.get("test"));
+    }
+
+    @Test
+    public void testRedisList(){
+        String listKey = "listKey";
+        jedis.lpush(listKey, "myLeftPush");
+        jedis.rpush(listKey, "myRightPush");
+        jedis.lpush(listKey, "myLeftPush");
+        logger.info("{}", jedis.lrange(listKey, 0, -1));
+        logger.info(jedis.lpop(listKey));
+        logger.info(jedis.lpop(listKey));
+        logger.info(jedis.lpop(listKey));
+    }
+
+    @Test
+    public void testRedisSet(){
+        String setKey = "setKey";
+        jedis.sadd(setKey, "sadd");
+        jedis.sadd(setKey, "sadd2");
+        jedis.sadd(setKey, "sadd3");
+        jedis.sadd(setKey, "sadd");
+        logger.info("{}", jedis.smembers(setKey));
+        logger.info("{}", jedis.sismember(setKey, "sadd4"));
+        logger.info("{}", jedis.sismember(setKey, "sadd"));
+        logger.info("{}", jedis.srem(setKey, "sadd4"));
+        logger.info("{}", jedis.srem(setKey, "sadd3"));
+        logger.info("{}", jedis.smembers(setKey));
+
+        // clean
+        jedis.srem(setKey, "sadd2");
+        jedis.srem(setKey, "sadd1");
+    }
+
+    @Test
+    public void testRedisHash() {
+        String hashKey = "hashKey";
+
+        jedis.hset(hashKey, "subKey1", "value1");
+        jedis.hset(hashKey, "subKey2", "value2");
+        jedis.hset(hashKey, "subKey1", "value1");
+        logger.info("{}", jedis.hgetAll(hashKey));
+        jedis.hdel(hashKey, "subKey2");
+        jedis.hdel(hashKey, "subKey2");
+        logger.info("{}", jedis.hget(hashKey,"subKey1"));
+        logger.info("{}", jedis.hgetAll(hashKey));
+
+        // clean
+        jedis.hdel(hashKey, "subKey1");
+    }
+
+    @Test
+    public void testRedisZset(){
+        String zsetKey = "zsetKey";
+
+        jedis.zadd(zsetKey, 5, "zsetKey1");
+        jedis.zadd(zsetKey, 3, "zsetkey2");
+        jedis.zadd(zsetKey, 3, "zsetkey2");
+        logger.info("{}", jedis.zrangeWithScores(zsetKey, 0, -1));
+        logger.info("{}", jedis.zrange(zsetKey, 0, -1));
+        logger.info("{}", jedis.zrangeByScoreWithScores(zsetKey, 0, 4));
+        jedis.zrem(zsetKey, "zsetKey1");
+        logger.info("{}", jedis.zrangeWithScores(zsetKey, 0, -1));
+        logger.info("{}", jedis.zrange(zsetKey, 0, -1));
+
+        // clean
+        jedis.zrem(zsetKey, "zsetKey2");
     }
 
     @Test
