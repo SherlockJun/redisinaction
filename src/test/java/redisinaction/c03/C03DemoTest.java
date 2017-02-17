f7b8423f301c2944541784c052e7c84585d39689
tanjianhui <tan_jianhui@163.com>
2017-02-17 13:43:35 +0800

    initial commit

--- src/test/java/redisinaction/c03/C03DemoTest.java
+++ src/test/java/redisinaction/c03/C03DemoTest.java
@@ -38,12 +38,68 @@ public class C03DemoTest extends BaseTest {
     }
 
     @Test
+    public void testRedisSet(){
+        String setKey01 = "setKey01";
+        String setKey02 = "setKey02";
+        String setKey03 = "setKey03";
+        jedis.sadd(setKey01, "a", "b", "c", "d");
+        jedis.sadd(setKey02, "c", "d", "e", "f");
+
+        logger.info("{}", jedis.sdiff(setKey01,setKey02));
+        logger.info("{}", jedis.sinter(setKey01,setKey02));
+        logger.info("{}", jedis.sunion(setKey01,setKey02));
+
+        logger.info("{}", jedis.sinterstore(setKey03, setKey01,setKey02));
+        logger.info("{}", jedis.smembers(setKey03));
+    }
+
+    @Test
     public void testRedisHash(){
         Map<String, String> elem = new HashMap<>();
 
         elem.put("hashKey01", "hashKey01Value");
         elem.put("hashKey02", "hashKey02Value");
+        elem.put("hashKey03", "1");
         jedis.hmset("hashKeyName01", elem);
         logger.info("{}", jedis.hmget("hashKeyName01", "hashKey01", "hashKey02"));
+
+        logger.info("{}", jedis.hlen("hashKeyName01"));
+
+        logger.info("{} {}", jedis.hexists("hashKeyName01", "hashKey00"), jedis.hexists("hashKeyName01", "hashKey01"));
+
+        logger.info("{}", jedis.hkeys("hashKeyName01"));
+
+        logger.info("{}", jedis.hvals("hashKeyName01"));
+
+        logger.info("{}", jedis.hgetAll("hashKeyName01"));
+
+        logger.info("{}", jedis.hincrByFloat("hashKeyName01", "hashKey03", .5));
+
+        logger.info("{}", jedis.hincrBy("hashKeyName01", "hashKey04", 5));
+    }
+
+    @Test void testRedisZset(){
+        String zsetKey = "zsetKey";
+        Map<String, Double> elem = new HashMap<>();
+
+        elem.put("member01", Double.valueOf(System.nanoTime()));
+        elem.put("member02", Double.valueOf(System.nanoTime()));
+        elem.put("member03", Double.valueOf(System.nanoTime()));
+        elem.put("member04", 1D);
+
+        jedis.zadd(zsetKey, elem);
+        logger.info("{}", jedis.zrange(zsetKey, 0, -1));
+
+        jedis.zrem(zsetKey, "member03");
+        logger.info("{}", jedis.zcard(zsetKey));
+
+        logger.info("{}", jedis.zincrby(zsetKey,2, "member04"));
+        logger.info("{}", jedis.zscore(zsetKey, "member04"));
+
+        logger.info("{}", jedis.zcount(zsetKey, -1, 5));
+        logger.info("{}", jedis.zcount(zsetKey, -1, 2));
+
+        logger.info("{}", jedis.zrank(zsetKey,"member01" ));
+        logger.info("{}", jedis.zrank(zsetKey,"member03" ));
     }
 }
